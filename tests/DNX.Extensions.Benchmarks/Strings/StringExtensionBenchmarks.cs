9dc2e26ec40ae36c9dd1c564eb6605944c4e5580
Martin Smith <martinsmith1968@gmail.com>
2025-01-12 17:26:56 +0000

    benchmark investigation

--- tests/DNX.Extensions.Benchmarks/Strings/StringExtensionBenchmarks.cs
+++ tests/DNX.Extensions.Benchmarks/Strings/StringExtensionBenchmarks.cs
@@ -2,6 +2,8 @@ using System.Diagnostics.CodeAnalysis;
 using BenchmarkDotNet.Attributes;
 using DNX.Extensions.Strings;
 
+// ReSharper disable UseStringInterpolation
+
 namespace DNX.Extensions.Benchmarks.Strings;
 
 [ExcludeFromCodeCoverage]
@@ -11,31 +13,39 @@ public class StringExtensionBenchmarks
 {
     private const string SearchText = nameof(SearchText);
 
-    private string _text = "";
+    private string _text1 = "";
+    private string _text2 = "";
 
-    [Params(100)]
-    public int StartTextSize { get; set; }
-
-    [Params(100)]
-    public int EndTextSize { get; set; }
+    [Params(50)]
+    public int AdditionalTextSize { get; set; }
 
     [GlobalSetup]
     public void GlobalSetup()
     {
-        _text = string.Format(
+        _text1 = string.Format(
             "{0}{1}{2}",
-            new string('-', StartTextSize),
+            new string('-', AdditionalTextSize),
             SearchText,
-            new string('-', EndTextSize)
+            new string('-', AdditionalTextSize)
         );
+
+        _text2 = SearchText;
+        for (var x = 0; x < AdditionalTextSize; ++x)
+            _text2 = "-+" + _text2 + "*-";
     }
 
     [Benchmark]
-    public string Before() => _text.Before(SearchText);
+    public string Before() => _text1.Before(SearchText);
 
     [Benchmark]
-    public string After() => _text.After(SearchText);
+    public string BeforeLast() => _text2.BeforeLast("+");
 
     [Benchmark]
-    public string Reverse() => _text.Reverse();
+    public string After() => _text1.After(SearchText);
+
+    [Benchmark]
+    public string AfterLast() => _text2.AfterLast("+");
+
+    [Benchmark]
+    public string Reverse() => _text1.Reverse();
 }
